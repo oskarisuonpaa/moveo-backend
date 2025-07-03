cabc9e6b6d6af5b55abad7f2632840efbb53be9b
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-07-03 13:55:15 +0300

    feat: add Zod validation middleware for non-empty request bodies and integrate it into calendar routes

--- src/controllers/calendar.controller.ts
+++ src/controllers/calendar.controller.ts
@@ -1,14 +1,58 @@
-import { NextFunction, Request, Response } from 'express';
-import { calendar } from '../services/googleCalendar.service';
+import { RequestHandler } from 'express';
+import { calendar, createCalendar } from '../services/googleCalendar.service';
+import { AppError } from '../middleware/error.middleware';
 
-export const getCalendars = async (
-  _request: Request,
-  response: Response,
-  next: NextFunction,
+export const getCalendars: RequestHandler = async (
+  _request,
+  response,
+  next,
 ) => {
   try {
     const calendarResponse = await calendar.calendarList.list();
     response.status(200).json({ data: calendarResponse.data.items });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getCalendar: RequestHandler = async (request, response, next) => {
+  const { calendarId } = request.params;
+  try {
+    const calendarResponse = await calendar.calendars.get({
+      calendarId,
+    });
+    response.status(200).json({ data: calendarResponse.data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface PostCalendarBody {
+  alias: string;
+}
+
+export const postCalendar: RequestHandler<
+  object,
+  unknown,
+  PostCalendarBody,
+  unknown
+> = async (request, response, next) => {
+  const { alias } = request.body;
+
+  if (!alias || typeof alias !== 'string') {
+    const error = new Error('Invalid calendar alias') as AppError;
+    error.status = 400;
+    return next(error);
+  }
+
+  const calendarData = {
+    summary: alias,
+    timeZone: 'Europe/Helsinki',
+  };
+
+  try {
+    const calendarResponse = await createCalendar(calendarData);
+    response.status(201).json({ data: calendarResponse });
   } catch (error) {
     next(error);
   }
--- src/routes/calendar.route.ts
+++ src/routes/calendar.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getCalendars } from '../controllers/calendar.controller';
+import { getCalendars, postCalendar } from '../controllers/calendar.controller';
+import { requireRequestBody } from '../middleware/validateRequestBody.middleware';
 
 const router = Router();
 
 router.get('/', getCalendars);
 // router.get('/:calendarId', getCalendar);
-// router.post('/', createCalendar);
+router.post('/', requireRequestBody, postCalendar);
 // router.delete('/:calendarId', deleteCalendar);
 // router.put('/:calendarId', updateCalendar);
 
