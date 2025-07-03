b8c45cb730bcf364985047ca0962706867687475
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-07-03 12:34:39 +0300

    refactor: comment out unused event and calendar route handlers

--- src/routes/calendar.route.ts
+++ src/routes/calendar.route.ts
@@ -4,5 +4,9 @@ import { getCalendars } from '../controllers/calendar.controller';
 const router = Router();
 
 router.get('/', getCalendars);
+// router.get('/:calendarId', getCalendar);
+// router.post('/', createCalendar);
+// router.delete('/:calendarId', deleteCalendar);
+// router.put('/:calendarId', updateCalendar);
 
 export default router;
--- src/routes/events.route.ts
+++ src/routes/events.route.ts
@@ -7,6 +7,9 @@ import {
 const router = Router();
 
 router.get('/:calendarId', getCalendarEvents);
+// router.get('/:calendarId/:eventId', getCalendarEvent);
 router.post('/:calendarId', postCalendarEvent);
+// router.delete('/:calendarId/:eventId', deleteCalendarEvent);
+// router.put('/:calendarId/:eventId', updateCalendarEvent);
 
 export default router;
