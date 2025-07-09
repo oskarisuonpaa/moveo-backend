478e3923de46d0c7f7db867268b2ae5d54b024da
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-07-09 10:23:47 +0300

    feat: restructure routes and add authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import errorHandler from './middleware/error.middleware';
-import calendarRouter from './routes/calendar.route';
-import eventsRouter from './routes/events.route';
-import attendeesRouter from './routes/events.attendees.route';
+import calendarRouter from './routes/calendar.routes';
+import eventsRouter from './routes/events.routes';
+import attendeesRouter from './routes/events.attendees.routes';
+import authRouter from './routes/auth.routes';
 import logger from '@utils/logger';
 import { AppError } from '@utils/errors';
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use('/api/calendars', calendarRouter);
 app.use('/api/events', eventsRouter);
 app.use('/api/events/attendees', attendeesRouter);
+app.use('/api/auth', authRouter);
 
 app.use((_request: Request, _response: Response, next: NextFunction) => {
   const error = new AppError('Not Found', 404);
