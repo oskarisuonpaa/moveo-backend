8952f9434467661efdbfc86acbf0b263ec88361f
Emilia Leino <emilia.leino@proton.me>
2025-07-21 15:23:01 +0300

    Added script to read product excel file into a json, updated seedData function

--- src/database/seedData.ts
+++ src/database/seedData.ts
@@ -1,12 +1,15 @@
 import { AppDataSource } from './data-source';
 import Product from '../models/product.model';
-import { mockProducts } from '../mocks/mockProducts';
+import fs from 'fs';
+
+const productsPath = '../mocks/products.json';
 
 // uses mockdata, update for production
 export async function seedProducts() {
   const productRepo = AppDataSource.getRepository(Product);
   const count = await productRepo.count();
   if (count === 0) {
+    const mockProducts = JSON.parse(fs.readFileSync(productsPath, 'utf-8'))
     await productRepo.save(mockProducts);
     console.log('Mock products inserted!');
   }
