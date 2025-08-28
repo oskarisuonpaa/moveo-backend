a00eff5b8888c684c4945ecbc09377f6463aa055
eleino <emilia.leino@proton.me>
2025-08-28 17:28:36 +0300

    -removed 'dependencies' dependency, added scheduling for imap bot

--- src/services/imap/index.ts
+++ src/services/imap/index.ts
@@ -2,6 +2,7 @@
 
 console.log('Script started');
 
+import cron from 'node-cron';
 import logger from '@utils/logger';
 import { connectToImap } from './imapclient';
 import AppError from '@utils/errors';
@@ -9,9 +10,9 @@ import AppError from '@utils/errors';
 logger.setLevel('debug');
 logger.setPrefix('IMAP');
 
-void (async () => {
-  logger.info('Connecting to email inbox...');
-
+// schedule to run every 15 minutes between 6 AM and 10 PM
+cron.schedule('*/15 6-22 * * *', async () => {
+  logger.info('Scheduled IMAP check started...');
   try {
     await connectToImap();
     logger.info('Email processing complete.');
@@ -20,4 +21,11 @@ void (async () => {
     logger.debug('Error details:', err);
     throw AppError.internal('IMAP connection failed', err);
   }
+});
+
+logger.info('IMAP email checker scheduled.');
+
+// Also run once at startup
+void (async () => {
+  await connectToImap();
 })();
